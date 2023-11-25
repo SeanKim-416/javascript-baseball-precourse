3a4e23926ad98560c1bc600f737dbab8538ef9b1
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-25 16:13:43 +0900

    feature: getResult 기능 구현

--- src/index.js
+++ src/index.js
@@ -14,12 +14,32 @@ const getUserInput = () => {
   return $userInput.value;
 };
 
+const getResult = (computerNum, myNum) => {
+  const hitData = {
+    strike: 0,
+    ball: 0,
+  };
+
+  for (const num of myNum) {
+    const numIndex = myNum.indexOf(num);
+    if (computerNum.indexOf(num) === numIndex) {
+      hitData.strike += 1;
+    } else if (computerNum.includes(num)) {
+      hitData.ball += 1;
+    }
+  }
+  console.log(hitData.strike, hitData.ball);
+};
+
 const play = () => {
   const userNum = getUserInput();
   if (!userNum) return;
 
   numArray[1] = userNum;
   console.log(numArray);
+
+  // 비교 수행
+  getResult(numArray[0], numArray[1]);
 };
 
 const $submitButton = document.querySelector('#submit');
