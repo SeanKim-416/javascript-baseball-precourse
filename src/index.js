61f6213606e13c69ad42ce0ba9633b1e4c3ea299
SeanKim-416 <56223156+SeanKim-416@users.noreply.github.com>
2023-11-25 14:59:35 +0900

    feature: getUserInput 함수와 이벤트 리스너 구현

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 import createComputerNum from './createComputerNum.js';
 
-console.log(createComputerNum());
+const COMPUTER_NUM = createComputerNum();
+const numArray = [COMPUTER_NUM, null];
+
+const getUserInput = () => {
+  const $userInput = document.querySelector('#user-input');
+  // 올바른 값인지 확인
+  return $userInput.value;
+};
+
+const $submitButton = document.querySelector('#submit');
+$submitButton.addEventListener('click', () => {
+  numArray[1] = getUserInput();
+  console.log(numArray);
+});
